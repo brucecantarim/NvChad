548b095af6b4bab6b29cacf425ecaff9cc92cece
Bruce Cantarim <bruce@cantarim.com>
2023-05-20 23:02:51 -0700

    feat(lspconfig)

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -5,7 +5,17 @@ local lspconfig = require("lspconfig")
 
 local servers = {
   html = {},
-  cssls = {},
+  cssls = {
+    cmd = { 'vscode-css-language-server', '--stdio' },
+    filetypes = { 'css', 'scss', 'less' },
+    root_dir = lspconfig.util.root_pattern('package.json', '.git'),
+    single_file_support = true,
+    settings = {
+      css = { validate = true },
+      scss = { validate = true },
+      less = { validate = true },
+    },
+  },
   tailwindcss = {},
   sqlls = {
     cmd = { "sql-language-server", "up", "--method", "stdio" }
